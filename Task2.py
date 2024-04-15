412b20e2e17545988e362e0e67a8441d1fdc4175
Devesh-45 <devesh.ashok02@gmail.com>
2024-04-15 23:19:30 +0530

    Task

--- Task2.py
+++ Task2.py
@@ -1,9 +1,15 @@
-a=int(input("Enter a num1:"))
+'''a=int(input("Enter a num1:"))
 b=int(input("Enter a num2:"))
 c=int(input("Enter a num3:"))
 d=max(a,b,c)
 for i in range(1,d+1):
     if i%a==0 and i%b==0:
-        print(i)
+        print(i)'''
+
+'''a=int(input())
+print(a)'''
+
+'''a,b,c="aAbBA3".split("A")
+print(a,b,c)'''
     
     
