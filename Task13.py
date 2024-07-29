11c8c0454a9f8aba7f127bb5169ced23b99018f1
Devesh-45 <devesh.ashok02@gmail.com>
2024-07-29 11:52:01 +0530

    Task completed

--- Task13.py
+++ Task13.py
@@ -1,6 +1,6 @@
 A=str(input())
-if len(A)==13 or len(A)==12:
-    if A[0:2].isalpha() and A[3:5].isdigit() and (A[6:7] or A[6:8]).isalpha and A[9:13].isdigit():
+if len(A)==10 or len(A)==9:
+    if A[0:2].isalpha() and A[2:4].isdigit() and (A[4:6] or A[4:5]).isalpha and A[6:10].isdigit():
         print("Valid")
     else:
         print("Not valid")
